dad38a80372dfaa102b92ed30691fbfac320e81a
JoanaDalmeida <celestina.pricilia@gmail.com>
2015-03-06 18:04:08 +0100

    Elastic search fix full-result error.

--- api/src/test/java/roldophe/demo/movie/MovieTest.java
+++ api/src/test/java/roldophe/demo/movie/MovieTest.java
@@ -1,9 +1,13 @@
 package roldophe.demo.movie;
 
+import io.vertigo.dynamo.domain.model.DtList;
+
 import javax.inject.Inject;
 
 import org.junit.Test;
 
+import rodolphe.demo.domain.movies.MovieCriteria;
+import rodolphe.demo.domain.movies.MovieResult;
 import rodolphe.demo.services.movie.MovieServices;
 import roldophe.demo.tools.AbstractRodolpheTestCase;
 
@@ -19,4 +23,13 @@ public class MovieTest extends AbstractRodolpheTestCase{
 		movieServices.getMovies("Fantastic");
 	}
 
+	/**
+	 * Test getMovies service.
+	 */
+	@Test
+	public void searchByTitle(){
+		final MovieCriteria crit = new MovieCriteria();
+		crit.setTitle("Fantastic");
+		final DtList<MovieResult> movies = movieServices.getMoviesByCriteria(crit);
+	}
 }
