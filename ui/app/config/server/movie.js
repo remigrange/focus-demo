02129cb8288eaa98e0ef22675fb58c1b050441af
KACIM Khalil <kacim.khalil@gmail.com>
2015-03-17 18:02:04 +0100

    Création du service et action pour movie

--- ui/app/config/server/movie.js
+++ ui/app/config/server/movie.js
@@ -4,5 +4,5 @@ module.exports = {
   getAll: url(root, 'GET'),
   update: url(root + "${id}/",'PUT'),
   create: url(root, 'POST'),
-  get: url(root + "${id}/", 'GET')
+  get: url(root + "${id}", 'GET')
 };
