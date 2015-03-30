3f799312e421853fd72d95351af21c959cc34b13
Pierre <pierre.besson7@gmail.com>
2015-03-30 23:18:33 +0200

    [clean] Remove useless file.

--- ui/app/initialize.jsx
+++
@@ -1,9 +0,0 @@
-/* global focus*/
-console.log(focus);
-console.log(focusComponents);
-focus.components = focusComponents;
-//Require dependencies.
-require('./initializer');
-require("router");
-Backbone.history.start();
-//focus.application.render(Hello, '#page', {props: {name: "rodolphe ....."}});
