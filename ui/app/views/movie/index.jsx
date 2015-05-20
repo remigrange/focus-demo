2a479adeac6eb9530c2f5d9c58bec17e29cd985a
Pierre Besson <pierre.besson7@gmail.com>
2015-05-20 19:48:29 +0200

    [detail][movie] Remove sliding content.

--- ui/app/views/movie/index.jsx
+++ ui/app/views/movie/index.jsx
@@ -1,7 +1,12 @@
 //Get the form mixin.
 var SlidingContent = require('./slidingContent');
 var StickyNavigation = Focus.components.common.stickyNavigation.component;
-
+var Title = Focus.components.common.title.component;
+var MovieDetails = require('./movieDetails');
+var Castings = require('./castings');
+var MovieProducers = require('./movieProducers');
+var MovieDirectors = require('./movieDirectors');
+var MoviePictures = require('./moviePictures');
 module.exports = React.createClass({
     render: function renderMovieView() {
         return (
