eef99377e37a3b7bf9d5ad7fb94af733e65b2926
Pierre Besson <pierre.besson7@gmail.com>
2015-05-21 18:46:43 +0200

    [detail] Add cartridge

--- ui/app/views/movie/index.jsx
+++ ui/app/views/movie/index.jsx
@@ -1,18 +1,27 @@
 
 //Récupération des dépendances.
-var Detail = Focus.components.common.detail.component
+var detailMixin = Focus.components.page.detail.mixin;
+var Detail = Focus.components.common.detail.component;
+
 //Blocs composants la page.
 var MovieDetails = require('./movieDetails');
 var Castings = require('./castings');
 var MovieProducers = require('./movieProducers');
 var MovieDirectors = require('./movieDirectors');
 var MoviePictures = require('./moviePictures');
-
+//Composants du cartouche
+var SummaryMovie = React.createClass({render: function(){<div>SUMMARY ..............</div>}});
+var CartridgeMovie = React.createClass({render: function(){<div>CARTRIDGE ..............</div>}});
 
 /**
  * Page représentant le détail de la fiche d'un film.
  */
 module.exports = React.createClass({
+    mixins: [detailMixin],
+    cartridgeConfiguration : {
+      summary: SummaryMovie,
+      cartridge: CartridgeMovie
+    },
     /** @inheritedDoc */
     displayName: 'MovieView',
     /** @inheritedDoc */
