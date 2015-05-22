81b2fc0f7787f884a96b3e26aa855801f10f7ba4
Pierre Besson <pierre.besson7@gmail.com>
2015-05-22 18:39:00 +0200

    [header] update

--- ui/app/views/header/index.jsx
+++ ui/app/views/header/index.jsx
@@ -1,6 +1,7 @@
 //Needed components
 var Header = Focus.components.application.header.component;
 var Cartridge = Focus.components.application.cartridge.component;
+var ContentBar = Focus.components.application.contentBar.component;
 var Bar = Focus.components.application.bar.component;
 
 module.exports = React.createClass({
@@ -8,8 +9,15 @@ module.exports = React.createClass({
   render: function renderApplicationHeader() {
     return (
       <Header>
-        <Bar appName='FOCUS'/>
-        <Cartridge />
+        <ContentBar>
+          <Bar appName='FOCUS'/>
+          <Cartridge />
+        </ContentBar>
+        <div data-focus='content-actions'>
+          <button className="btn btn-success btn-fab btn-raised mdi-action-language"></button>
+          <button className="btn btn-danger btn-fab btn-raised mdi-social-notifications"></button>
+          <button className="btn btn-primary btn-fab btn-raised mdi-action-account-circle"></button>
+        </div>
       </Header>
     );
   }
