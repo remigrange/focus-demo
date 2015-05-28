bc4c4df4ec68e4256796aab961099da8e0956441
Thomas <tgallon@gmail.com>
2015-05-28 19:55:09 +0200

    [detail] list of people icon

--- ui/app/views/people/movieCard.jsx
+++ ui/app/views/people/movieCard.jsx
@@ -2,7 +2,7 @@ module.exports = React.createClass({
     render: function renderMovieCard() {
         return (
             <div className='card'>
-                <div className='picture'><img src="./static/img/logoMovie.png" width="100%" height="100%"/></div>
+                <div className='picture'><img src="./static/img/logoMovie.png" /></div>
                 <div>
                   <div className='name'>{this.props.name}</div>
                   <div className='middleName'>{this.props.middleName}</div>
