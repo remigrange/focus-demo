0d6e6c28f2333ba1635c4950930362bf1a79ea5c
Thomas <tgallon@gmail.com>
2015-05-28 19:54:32 +0200

    [detail] correction of list of people style

--- ui/app/views/movie/component/peopleCard.jsx
+++ ui/app/views/movie/component/peopleCard.jsx
@@ -1,10 +1,10 @@
 module.exports = React.createClass({
     render: function renderPeopleCard() {
         return (
-            <div className='card'>
-                <div className='picture'><img src="./static/img/peopleLogo.png" width="100%" height="100%"/></div>
-                <div className='name'>{this.props.name}</div>
-                <div className='subName'>{this.props.subName}</div>
+            <div className='people-line'>
+                <div className='icon'><i className="fa fa-user fa-3x"></i></div>
+                <div className='name level1'>{this.props.name}</div>
+                <div className='subName level2'>{this.props.subName}</div>
             </div>
         );
     }
