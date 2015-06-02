de67d657fa971538bc1c196cb70127a2f9ba6afa
Pierre Besson <pierre.besson7@gmail.com>
2015-06-02 09:54:00 +0200

    [detail] Update cartridge and view

--- ui/app/views/movie/cartridge.jsx
+++ ui/app/views/movie/cartridge.jsx
@@ -1,75 +1,39 @@
+/*global React*/
 var formMixin = Focus.components.common.form.mixin;
 var movieActions = require('../../action/movie');
 var movieStore = require('../../stores/movie');
+
+var Field = React.createClass({
+  getDefaultProps: function(){
+    return {
+      title: ''
+    };
+  },
+  render: function renderField(){
+    return (
+      <div className='field'>
+        <div className='title'>{this.props.title}</div>
+        <div className='content'>{this.props.children}</div>
+      </div>
+    );
+  }
+
+});
+
+
 module.exports = React.createClass({
-    definitionPath: "movie",
-    displayName: "cartridge",
-    getInitialState: function () {
-        return {
-          actors: [],
-          producers: [],
-          directors: []
-        };
-    },
+    definitionPath: 'movie',
+    displayName: 'MovieCartridge',
     mixins: [formMixin],
-    stores: [{store: movieStore, properties: ["movie"]}],
+    stores: [{store: movieStore, properties: ['movie']}],
     action: movieActions,
     renderContent: function renderMovieCartridge() {
         return (
-            <div className="cartridge">
-                <div className="header">
-                    <div className="picture"><img src="./static/img/logoMovie.png" /></div>
-                    <div className="title">{this.state.title}</div>
-                    <div className="year">{this.state.year}</div>
-                </div>
-                <div className="field">
-                    <div className="title">{"GENRES"}</div>
-                    <div className="content">
-                        {this.state.genreIds}
-                    </div>
-                </div>
-                <div className="field">
-                    <div className="title">{"DIRECTORS"}</div>
-                    <div className="content">
-                        {this.state.directors.map(function (people) {
-                            return (
-                                <div>{people.peoName}</div>
-                            )
-                        })}
-                    </div>
-                </div>
-                <div className="field">
-                    <div className="title">{"PRODUCERS"}</div>
-                    <div className="content">
-                        {this.state.producers.map(function (people) {
-                            return (
-                                <div>{people.peoName} {people.comment}</div>
-                            )
-                        })}
-                    </div>
-                </div>
-                <div className="field">
-                    <div className="title">{"MAIN ACTORS"}</div>
-                    <div className="content">
-                        {this.state.actors.map(function (people) {
-                            return (
-                                <div>{people.peoName}</div>
-                            )
-                        })}
-                    </div>
-                </div>
-                <div className="field">
-                    <div className="title">{"COUNTRIES"}</div>
-                    <div className="content">
-                        {this.state.countryIds}
-                    </div>
-                </div>
-                <div className="field">
-                    <div className="title">{"LANGUAGES"}</div>
-                    <div className="content">
-                        {this.state.languageIds}
-                    </div>
-                </div>
+            <div className="movie-cartridge">
+              <div className='picture'><img src="./static/img/logoMovie.png" /></div>
+              <div className='title'>{this.state.title}</div>
+              <div className='year'>{this.state.year}</div>
+              <div className='country'>{this.state.countryIds}</div>
             </div>
         );
     }
