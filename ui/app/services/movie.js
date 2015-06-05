9da86c21e86d94f931056da25f7b9579bf6724bc
Stanislas Bernard <bernardstanislas@gmail.com>
2015-06-05 18:21:35 +0200

    Add movie posters

--- ui/app/services/movie.js
+++ ui/app/services/movie.js
@@ -1,25 +1,42 @@
 var URL = require('../../config/server');
 var fetch = Focus.network.fetch;
+
 module.exports = {
-    getMovieById: function getMovieById(id){
-        return fetch(URL.movie.get({urlData:{id: id}}));
+    getMovieById(id) {
+        return fetch(URL.movie.get({urlData: {id: id}}))
     },
-    getMovieViewById: function getMovieViewById(id){
-        return fetch(URL.movie.movieView({urlData:{id: id}}));
+    getMovieViewById(id) {
+        return fetch(URL.movie.movieView({urlData: {id: id}})).then((data) => {
+            return new Promise((success) => {
+                let xmlhttp = new XMLHttpRequest();
+                let url = URL.movie.poster({urlData: {title: data.title}});
+                xmlhttp.onload  = () => {
+                    if (xmlhttp.status == 200) {
+                        let poster = JSON.parse(xmlhttp.response).Poster;
+                        if (poster != 'N/A') {
+                            data.poster = poster;
+                        }
+                        success(data);
+                    }
+                };
+                xmlhttp.open(url.method, url.url, true);
+                xmlhttp.send();
+            });
+        });
     },
-    getMovieCastingsById: function getMovieCastingsById(id){
-        return fetch(URL.movie.castings({urlData:{id: id}}));
+    getMovieCastingsById(id) {
+        return fetch(URL.movie.castings({urlData: {id: id}}));
     },
 
-  getMovieProducersById: function getMovieProducersById(id){
-    return fetch(URL.movie.producers({urlData:{id: id}}));
-  },
+    getMovieProducersById(id) {
+        return fetch(URL.movie.producers({urlData: {id: id}}));
+    },
 
-  getMovieDirectorsById: function getMovieDirectorsById(id){
-    return fetch(URL.movie.directors({urlData:{id: id}}));
-  },
+    getMovieDirectorsById(id) {
+        return fetch(URL.movie.directors({urlData: {id: id}}));
+    },
 
-  updateMovie: function updateMovie(jsonMovie) {
-    return fetch(URL.movie.update({urlData:{id: jsonMovie.movId}, data: jsonMovie}));
-  }
+    updateMovie(jsonMovie) {
+        return fetch(URL.movie.update({urlData: {id: jsonMovie.movId}, data: jsonMovie}));
+    }
 };
