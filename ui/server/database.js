fcd64c74673097e5a5679b822c64889f8e84fe18
Stanislas Bernard <bernardstanislas@gmail.com>
2015-06-08 00:55:03 +0200

    Add elements in database

--- ui/server/database.js
+++ ui/server/database.js
@@ -1,5 +1,35 @@
 module.exports = {
-    scopes: [{
-
-    }]
+    scopes: [
+        {
+            code: 'MOVIES',
+            label: 'Films',
+            active: true
+        },
+        {
+            code: 'PEOPLE',
+            label: 'Personnes',
+            active: true
+        }
+    ],
+    movies: [
+        {
+            'movId': 985154,
+            'title': 'Le groupe',
+            'released': '2001-09-12T00:00:00.000Z',
+            'year': 2001,
+            'metadasJson': '\'Le groupe\' (2001) {Conspiration (#1.12)}'
+        }
+        // TODO : fill in more data
+    ],
+    movieDetails: [
+        {
+            'movId': 985154,
+            'title': 'Le groupe',
+            'released': '2001-09-12T00:00:00.000Z',
+            'genreIds': 'Comedy|Drama|Romance',
+            'countryIds': 'France',
+            'languageIds': 'French'
+        }
+        // TODO : fill in more data
+    ]
 };
