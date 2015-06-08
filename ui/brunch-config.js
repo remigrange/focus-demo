40379dda54fa73b723e81074c291a15e00bee3a9
Pierre Besson <pierre.besson7@gmail.com>
2015-06-08 17:10:17 +0200

    [config] fix brunch config

--- ui/brunch-config.js
+++ ui/brunch-config.js
@@ -10,27 +10,6 @@ exports.config = {
     path: './server/server.js'
   },
   files: {
-    javascripts: {
-      joinTo: {
-        'javascripts/app.js': /^app/,
-        'javascripts/vendor.js': /^(bower_components|vendor|node_modules)/
-      },
-      order: {
-        before: [
-            'vendor/react.js',
-            'vendor/jquery.js',
-            'vendor/bootstrap.min.js',
-            'vendor/ripples.min.js',
-            'vendor/material.min.js',
-            'vendor/lodash.js',
-            'vendor/backbone.js',
-            'vendor/focus.js',
-            'vendor/focus-components.js'
-        ],
-        after: ['vendor/picker.js']
-      }
-    },
-    files: {
         javascripts: {
             joinTo: {
                 'javascripts/app.js': /^app/,
