ece8957a534f0fa76474089e4b1bd7a375219606
Stanislas Bernard <bernardstanislas@gmail.com>
2015-06-08 14:07:53 +0200

    Fix require of components

--- ui/app/views/search/previews/movie-preview.js
+++ ui/app/views/search/previews/movie-preview.js
@@ -1,5 +1,5 @@
-let MovieCartridge = require('views/movie/cartridge');
-let MovieDetails = require('views/movie/movieDetails');
+let MovieCartridge = require('views/movie/movie-cartridge');
+let MovieDetails = require('views/movie/movie-details');
 
 let MoviePreview = React.createClass({
     render() {
