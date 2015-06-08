76f16d164537a57f8f4e1c0b94444e70c3c4a5a8
Stanislas Bernard <bernardstanislas@gmail.com>
2015-06-08 14:56:34 +0200

    Refactor people pictures

--- ui/app/views/people/people-pictures.jsx
+++ ui/app/views/people/people-pictures.jsx
@@ -1,11 +1,12 @@
-var Title = Focus.components.common.title.component;
+let Title = Focus.components.common.title.component;
+
 module.exports = React.createClass({
-  displayName: "moviePictures",
-  render: function render() {
-    return (
-      <div className='slidingBloc noBorderBottom'>
-        <Title id="pictures" title="PICTURES"/>
-      </div>
-    );
-  }
+    displayName: 'moviePictures',
+    render() {
+        return (
+            <div className='slidingBloc noBorderBottom'>
+                <Title id='pictures' title='PICTURES'/>
+            </div>
+        );
+    }
 });
