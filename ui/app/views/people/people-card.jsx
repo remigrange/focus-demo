2dfcb131b0685c5c0212bef2b386997c92149dfe
Stanislas Bernard <bernardstanislas@gmail.com>
2015-06-08 14:23:16 +0200

    Move people card

---
+++ ui/app/views/people/people-card.jsx
@@ -0,0 +1,11 @@
+module.exports = React.createClass({
+    render() {
+        return (
+            <div className='people-line'>
+                <div className='icon'><i className="fa fa-user fa-3x"></i></div>
+                <div className='name level1'>{this.props.name}</div>
+                <div className='subName level2'>{this.props.subName}</div>
+            </div>
+        );
+    }
+});
