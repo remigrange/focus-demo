5ed5d0ef877b5a62c5f34134fd5f9bb6d9baa0ac
Pierre Besson <pierre.besson7@gmail.com>
2015-06-11 08:39:12 +0200

    [router] Add a home router.

---
+++ ui/app/router/home-router.js
@@ -0,0 +1,18 @@
+/*global Backbone, Focus, Focus.components */
+//Dependencies.
+let Router = Focus.router;
+let HomeRouter = Router.extend({
+    log: true,
+    routes: {
+      '': 'home',
+      'home': 'home'
+    },
+    home() {
+        Focus.application.changeRoute('home');
+        console.log('ROUTE: HOME');
+        let HomeView = require('../views/home');
+        this._pageContent(HomeView, {props: {position: 'left', open: true, style: {className: 'home-popin'}}});
+    }
+});
+
+module.exports = new HomeRouter();
