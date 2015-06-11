89f37107ca1297fbbf9b5178b2a318d88868f86e
Pierre Besson <pierre.besson7@gmail.com>
2015-06-11 17:34:04 +0200

    [domains] Clean all domains.

--- ui/app/config/domain/index.js
+++ ui/app/config/domain/index.js
@@ -1,24 +1,32 @@
+/*global moment*/
 module.exports = {
+    DO_COMMENTAIRE_NOT_ANALYZED: require('./do-commentaire-not-analyzed'),
     DO_LIBELLE_50: require('./do-libelle-50'),
     DO_LIBELLE_250: require('./do-libelle-250'),
     DO_LIBELLE_100: require('./do-libelle-100'),
     DO_TEXTE: require('./do-texte'),
-    DO_BOOLEEN: {
+    DO_TAILLE_FICHIER: require('./do-taille-fichier'),
+    DO_IDENTIFIANT_CODE: require('./do-identifiant-code'),
+    DO_MULTI_VALUES_FIELD: require('./do-multi-values-field'),
+    DO_DT_PEOPLE_DTC: require('./do-dt-people-dtc'),
+    DO_NOM: require('./do-nom'),
+    DO_PRENOM: require('./do-prenom'),
+    DO_PASSWORD: require('./do-password'),
+    /*DO_BOOLEEN: {
         "type": "boolean"
-    },
-    "DO_DATE": {
+    },*/
+    DO_DATE: {
         "type": "text",
-        "decorator": "datePicker",
         "style": "date right",
-        "format": {
-            "value": function(data){return data;}
-        },'InputComponent': Focus.components.common.input.date.component,
-        'formatter': function(date){
+        format: {
+            "value": function dateFormat(data){return data;}
+        },
+        'InputComponent': Focus.components.common.input.date.component,
+        formatter: function dateFormatter(date){
             return moment(date).format('L')
         }
-
     },
-    "DO_MONTANT": {
+    /*"DO_MONTANT": {
         "type": "number",
         "validator": [{
             "type": "number",
@@ -28,7 +36,7 @@ module.exports = {
         "format": {
             "value": function(data){return data;}
         }
-    },
+    },*/
     "DO_EMAIL": {
         "type": "email",
         "inputMaxLength": 150,
@@ -51,87 +59,13 @@ module.exports = {
     "DO_ID": {
         "type": "text"
     },
-    "DO_LISTE_MULTI": {
-        "type": "text",
-        "decorator": "select2"
-    },
-    "DO_ACTIF": {
+    /*"DO_ACTIF": {
         "type": "boolean"
-    },
-    "DO_ANNEE": {
+    },*/
+    /*"DO_ANNEE": {
         "type": "number"
-    },
-    "DO_CHAMP_ADRESSE": {
-        "type": "text",
-        "inputMaxLength": 50,
-        "validator": [{
-            "type": "string",
-            "options": {
-                "maxLength": 50
-            }
-        }]
-    },
-    "DO_CHEMIN_ACCES": {
-        "type": "text",
-        "inputMaxLength": 250,
-        "validator": [{
-            "type": "string",
-            "options": {
-                "maxLength": 250
-            }
-        }]
-    },
-    "DO_CODE_1": {
-        "type": "text",
-        "inputMaxLength": 1,
-        "validator": [{
-            "type": "string",
-            "options": {
-                "maxLength": 1
-            }
-        }]
-    },
-    "DO_CODE_3": {
-        "type": "text",
-        "inputMaxLength": 3,
-        "validator": [{
-            "type": "string",
-            "options": {
-                "maxLength": 3
-            }
-        }]
-    },
-    "DO_CODE_4": {
-        "type": "text",
-        "inputMaxLength": 4,
-        "validator": [{
-            "type": "string",
-            "options": {
-                "maxLength": 4
-            }
-        }]
-    },
-    "DO_CODE_10": {
-        "type": "text",
-        "inputMaxLength": 10,
-        "validator": [{
-            "type": "string",
-            "options": {
-                "maxLength": 10
-            }
-        }]
-    },
-    "DO_CODE_20": {
-        "type": "text",
-        "inputMaxLength": 20,
-        "validator": [{
-            "type": "string",
-            "options": {
-                "maxLength": 20
-            }
-        }]
-    },
-    "DO_CODE_POSTAL": {
+    },*/
+    /*"DO_CODE_POSTAL": {
         "type": "text",
         "inputMaxLength": 9,
         "validator": [{
@@ -140,7 +74,7 @@ module.exports = {
                 "maxLength": 9
             }
         }]
-    },
+    },*/
     "DO_COMMENTAIRE": {
         "type": "textarea",
         "validator": [{
@@ -150,13 +84,23 @@ module.exports = {
             }
         }]
     },
-    "DO_DATE_INC": {
+    /*"DO_DATE_INC": {
         "type": "text",
         "validator": [{
             "type": "regex",
             "value": /^((0[1-9]|[12][0-9]|3[01])\/(0[1-9]|1[012])\/(19[0-9][0-9]|20[0-9][0-9]))|((0[1-9]|1[012])\/(19[0-9][0-9]|20[0-9][0-9]))|(19[0-9][0-9]|20[0-9][0-9])$/,
             "options": {
                 "translationKey": "Veuillez saisir une date au format DD/MM/AAAA, MM/AAAA ou AAAA."
+            }
+        }]
+    },*/
+    "DO_CHEMIN_ACCES": {
+        "type": "text",
+        "inputMaxLength": 250,
+        "validation": [{
+            "type": "string",
+            "options": {
+                "maxLength": 250
             }
         }]
     },
@@ -168,43 +112,7 @@ module.exports = {
             "value": function(data){return data;}
         }
     },
-    "DO_DECIMAL_3": {
-        "type": "number"
-    },
-    "DO_DECIMAL_5": {
-        "type": "number"
-    },
-    "DO_DEPARTEMENT": {
-        "type": "number"
-    },
-    "DO_CD": {
-        "type": "text",
-        "validator": [{
-            "type": "string",
-            "options": {
-                "maxLength": 18
-            }
-        }]
-    },
-    "DO_ID_BADGE": {
-        "type": "text",
-        "validator": [{
-            "type": "string",
-            "options": {
-                "maxLength": 13
-            }
-        }]
-    },
-    "DO_MDP": {
-        "type": "password",
-        "validator": [{
-            "type": "string",
-            "options": {
-                "maxLength": 15
-            }
-        }]
-    },
-    "DO_LIBELLE_COURT": {
+    /*"DO_LIBELLE_COURT": {
         "type": "text",
         "inputMaxLength": 50,
         "validator": [{
@@ -236,29 +144,7 @@ module.exports = {
             }
         }],
         "style": "texte_250"
-    },
-    "DO_LIBELLE_FACIAL": {
-        "type": "text",
-        "inputMaxLength": 40,
-        "validator": [{
-            "type": "string",
-            "options": {
-                "maxLength": 40
-            }
-        }],
-        "style": "texte_40"
-    },
-    "DO_MATRICULE": {
-        "type": "text",
-        "inputMaxLength": 10,
-        "validator": [{
-            "type": "string",
-            "options": {
-                "maxLength": 10
-            }
-        }]
-    },
-    "DO_TEL": {
+    },"DO_TEL": {
         "type": "text",
         "inputMaxLength": 13,
         "validator": [{
@@ -282,5 +168,5 @@ module.exports = {
             }
         }],
         "style": "texte_13"
-    }
+    }*/
 };
