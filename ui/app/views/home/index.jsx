f88dd6465ff157d8cf19c4b2002261864cdaf8e4
Thomas <thomas.gallon@gmail.com>
2015-06-12 23:10:56 +0200

    home page : add search bar

--- ui/app/views/home/index.jsx
+++ ui/app/views/home/index.jsx
@@ -1,7 +1,35 @@
-let headerBehaviour = Focus.components.page.mixin
+let headerBehaviour = Focus.components.page.mixin;
+let CartridgeBehaviour = Focus.components.page.mixin.cartridgeBehaviour;
+let CartridgeSearch = require('../common/cartridge-search');
+let SummarySearch = require('../common/summary-search');
+
+// Composants du cartouche
+let ApplicationTitle = React.createClass({
+    render() {
+        return (
+            <span className="page-title">FOCUS</span>
+        );
+    }
+});
+
+
 /* global React, Focus.components */
 module.exports = React.createClass({
-  mixins: [headerBehaviour],
+  mixins: [headerBehaviour, CartridgeBehaviour],
+
+ cartridgeConfiguration: function cartridgeConfiguration() {
+    var cartridgeConfiguration = {
+      barLeft:{component: ApplicationTitle},
+      summary: {component: SummarySearch},
+      cartridge: {component: CartridgeSearch},
+      actions: {
+          primary: [],
+          secondary: []
+      }
+    };
+    return cartridgeConfiguration;
+  },
+
   render: function renderPeopleView() {
     return (
       <div className="homepage"></div>
