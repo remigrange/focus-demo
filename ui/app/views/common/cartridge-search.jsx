6813ebcbc2d2f4bf79901aa532a8ab80bdae8fa2
Pierre Besson <pierre.besson7@gmail.com>
2015-06-12 18:19:41 +0200

    [cartridge] Search add title and summary

--- ui/app/views/common/cartridge-search.jsx
+++ ui/app/views/common/cartridge-search.jsx
@@ -2,7 +2,10 @@ let QuickSearch = Focus.components.search.searchBar.component;
 module.exports = React.createClass({
     render() {
         return (
-            <QuickSearch />
+            <div className='cartridge-search'>
+              <h1>Que recherchez vous</h1>
+              <QuickSearch />
+            </div>
         );
     }
 });
