411ae219ed67c8678b50e23d2f0449f4d86c710d
Stanislas Bernard <bernardstanislas@gmail.com>
2015-06-12 16:14:26 +0200

    [quick-search] Add i18n on most relevant message

--- ui/app/views/search/quick-search/index.jsx
+++ ui/app/views/search/quick-search/index.jsx
@@ -20,6 +20,10 @@ let QuickSearch = Focus.components.page.search.quickSearch.component;
 let searchAction = require('action/search');
 let scopeAction = require('action/scope');
 
+// Mixins
+
+let i18nMixin = Focus.components.common.i18n.mixin;
+
 // Stores
 
 let searchStore = require('stores/search');
@@ -68,6 +72,7 @@ let QuickSearchWrapper = React.createClass({
         let popinCloser = this.props.closePopin;
         let self = this;
         return React.createClass({
+            mixins: [i18nMixin],
             _advancedSearchClickHandler(scope) {
                 return () => {
                     let route = `#search/advanced/scope/${scope}${self._query ?  '/query/' + self._query : ''}`;
@@ -87,7 +92,7 @@ let QuickSearchWrapper = React.createClass({
                             <Button handleOnClick={this._advancedSearchClickHandler(this.props.groupKey)} label='button.advancedSearch'
                                     shape="ghost"></Button>
                             <h3>{`${this.props.groupKey} (${numberFormatter.format(count, '(0,0)')})`}</h3>
-                            <p>Les 3 plus pertinents</p>
+                            <p>{this.i18n('search.mostRelevant')}</p>
                         </div>
                         {this.props.children}
                     </div>
