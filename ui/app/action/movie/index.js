977bd04ac15f9f0870bcb1eca40d9054965bc023
Thomas <tgallon@gmail.com>
2015-06-18 15:02:09 +0200

    [movie] fix header style

--- ui/app/action/movie/index.js
+++ ui/app/action/movie/index.js
@@ -1,37 +1,41 @@
 var movieServices = require('../../services').movie;
 var actionBuilder = Focus.application.actionBuilder;
 module.exports = {
+    action: {
+        group: 'Group by',
+        ungroup: 'Ungroup'
+    },
     movie: {
-      load: actionBuilder({
-        service: movieServices.getMovieViewById,
-        node: 'movie',
-        status: 'loaded'
-      }),
-      save: actionBuilder({
-        service: movieServices.updateMovie,
-        node: 'movie',
-        status: 'saved'
-      })
+        load: actionBuilder({
+            service: movieServices.getMovieViewById,
+            node: 'movie',
+            status: 'loaded'
+        }),
+        save: actionBuilder({
+            service: movieServices.updateMovie,
+            node: 'movie',
+            status: 'saved'
+        })
     },
     castings: {
-      load: actionBuilder({
-        service: movieServices.getMovieCastingsById,
-        node: 'castings',
-        status: 'loaded'
-      })
+        load: actionBuilder({
+            service: movieServices.getMovieCastingsById,
+            node: 'castings',
+            status: 'loaded'
+        })
     },
     producers: {
-      load: actionBuilder({
-        service: movieServices.getMovieProducersById,
-        node: 'producers',
-        status: 'loaded'
-      })
+        load: actionBuilder({
+            service: movieServices.getMovieProducersById,
+            node: 'producers',
+            status: 'loaded'
+        })
     },
     directors: {
-      load: actionBuilder({
-        service: movieServices.getMovieDirectorsById,
-        node: 'directors',
-        status: 'loaded'
-      })
+        load: actionBuilder({
+            service: movieServices.getMovieDirectorsById,
+            node: 'directors',
+            status: 'loaded'
+        })
     }
 };
