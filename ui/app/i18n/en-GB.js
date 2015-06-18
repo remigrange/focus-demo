02fb6350e7559a345ef17439362271029caa9c7f
Thomas <tgallon@gmail.com>
2015-06-18 14:41:07 +0200

    [People view] traductions

--- ui/app/i18n/en-GB.js
+++ ui/app/i18n/en-GB.js
@@ -70,6 +70,9 @@ module.exports = {
         detail: {
             identity: {
                 title: 'Identity'
+            },
+            filmography: {
+                title: 'Filmography'
             }
         }
     },
