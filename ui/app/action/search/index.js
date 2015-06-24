05d6a24aab1eac5cad6bfd80b2aeec994c67d068
Stanislas Bernard <bernardstanislas@gmail.com>
2015-06-24 11:16:03 +0200

    [search-action] Force a * when the search query is empty

--- ui/app/action/search/index.js
+++ ui/app/action/search/index.js
@@ -98,6 +98,13 @@ module.exports = {
 
         let criteria = param.criteria;
 
+        // Check if query is null, replace with * if so
+
+        if (criteria.query === '') {
+          criteria.query = '*';
+        }
+
+
         // Ordering and pagination
         let urlData = assign(_buildPagination(param.pageInfos), _buildOrderAndSort(param.pageInfos));
 
@@ -137,71 +144,5 @@ module.exports = {
                 return _parseScopedResponse(data, {scope: criteria.scope, page: urlData.page})
             })
             .then(_dispatchResult);
-    },
-    searchDeprecated: function (criteria) {
-        //criteria.pageInfos.page = criteria.pageInfos.page || 0;
-        //criteria.pageInfos.skip = criteria.pageInfos.skip || 0;
-        //criteria.group = criteria.pageInfos.group;
-        //if (criteria.group === undefined || criteria.group === null) {
-        //    criteria.group = '';
-        //}
-        //if (criteria.pageInfos.order !== undefined) {
-        //    criteria.pageInfos.sortFieldName = criteria.pageInfos.order.key;
-        //    if (criteria.pageInfos.order.order !== undefined && criteria.pageInfos.order.order !== null) {
-        //        if (criteria.pageInfos.order.order.toLowerCase() === 'asc') {
-        //            criteria.pageInfos.sortDesc = false;
-        //        } else if (criteria.pageInfos.order.order.toLowerCase() === 'desc') {
-        //            criteria.pageInfos.sortDesc = true;
-        //        }
-        //    }
-        //} else {
-        //criteria.pageInfos.sortFieldName = undefined;
-        //criteria.pageInfos.sortDesc = undefined;
-        //}
-        return services.search.searchByScope(criteria).then(
-            function success(data) {
-                if (data.facets) {
-                    data.facets = keys(data.facets).reduce((liveFilterFacets, serverFacetKey) => {
-                        let serverFacetData = data.facets[serverFacetKey];
-                        liveFilterFacets[serverFacetKey] = keys(serverFacetData).reduce((facetData, serverFacetItemKey) => {
-                            let serverFacetItemValue = serverFacetData[serverFacetItemKey];
-                            facetData[serverFacetItemKey] = {
-                                label: serverFacetItemKey,
-                                count: serverFacetItemValue
-                            };
-                            return facetData;
-                        }, {});
-                        return liveFilterFacets;
-                    }, {});
-                }
-                var dataRet = {
-                    facet: data.facets,
-                    map: data.map,
-                    pageInfos: {
-                        currentPage: criteria.pageInfos.page,
-                        perPage: 50,
-                        totalRecords: data.totalRecords
-                    }
-                };
-                if (criteria.group) {
-                    dataRet.pageInfos = {};
-                }
-                if (data.records) {
-                    Focus.dispatcher.handleServerAction(
-                        {
-                            data: {
-                                movieRecords: data.records.MOVIE,
-                                peopleRecords: data.records.PEOPLE
-                            },
-                            type: 'update'
-                        }
-                    );
-                }
-                Focus.dispatcher.handleServerAction({data: dataRet, type: 'update'});
-            },
-            function error(errors) {
-                console.info('Errrors: ', errors);
-            }
-        );
     }
 };
