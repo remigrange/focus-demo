020ed8a3f05d7c63599303ae10c88609b19da24e
Stanislas Bernard <bernardstanislas@gmail.com>
2015-06-30 13:36:13 +0200

    Cleanup

--- ui/app/views/search/advanced-search/column-config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  orderableColumnList: [
-      {key: 'TITLE_SORT_ONLY', order: 'desc', label: 'Title desc'},
-      {key: 'TITLE_SORT_ONLY', order: 'asc', label: 'Title asc'},
-      {key: 'GENRE_IDS', order: 'desc', label: 'Genre desc'},
-      {key: 'GENRE_IDS', order: 'asc', label: 'Genre asc'}
-  ]
-};
