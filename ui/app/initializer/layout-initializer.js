86a55c5330dd579131545787da370a5bc33eccbb
Stanislas Bernard <bernardstanislas@gmail.com>
2015-07-07 13:37:33 +0200

    [layout] Use new layout prop

--- ui/app/initializer/layout-initializer.js
+++ ui/app/initializer/layout-initializer.js
@@ -1,7 +1,8 @@
 let render = Focus.application.render;
 let Layout = Focus.components.application.layout.component;
 let MenuLeft = require('../views/menu');
+let footerText = 'Focus-démo - Propulsé par la Team Focus \u00A9 KleeGroup 2015';
 
 render(Layout, 'body', {
-    props: {MenuLeft}
+    props: {MenuLeft, footerText}
 });
